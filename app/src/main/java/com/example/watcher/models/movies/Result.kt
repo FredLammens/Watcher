3008e3d4e316f7fc376c7612c476b27cd82b8d71
Biebem <lammens.f96@gmail.com>
2020-12-18 04:41:05 +0100

    viewmodel mapped the relative url to the images

--- app/src/main/java/com/example/watcher/models/movies/Result.kt
+++ app/src/main/java/com/example/watcher/models/movies/Result.kt
@@ -35,7 +35,7 @@ data class Result(
     val overview: String,
     val popularity: Double,
     @Json(name = "poster_path")
-    val posterPath: String,
+    var posterPath: String,
     @Json(name = "release_date")
     val releaseDate: String,
     val title: String,
