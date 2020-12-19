c319c846d4a3d3e2124130692530c34942b19897
Biebem <lammens.f96@gmail.com>
2020-12-19 15:15:59 +0100

    Added Room   + repository class

---
+++ app/src/main/java/com/example/watcher/data/MoviesRepository.kt
@@ -0,0 +1,6 @@
+package com.example.watcher.data
+
+import com.example.watcher.data.local.MoviesDatabase
+
+class MoviesRepository(val db: MoviesDatabase) {
+}
---
+++ app/src/main/java/com/example/watcher/data/local/MoviesDatabase.kt
@@ -0,0 +1,32 @@
+package com.example.watcher.data.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.watcher.models.movies.Result
+
+@Database(
+        entities = [Result::class],
+        version = 1
+)
+abstract class MoviesDatabase : RoomDatabase(){
+
+    abstract fun getMoviesDao(): MovieDao
+
+
+    companion object{ //creates actual database
+
+        @Volatile private var instance: MoviesDatabase? = null //volatile = other threads see when instance changes / instance = singleton
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK){//synchronize lock makes sure theres only one database/ not another threads that sets this
+            instance ?: createDatabase(context).also{instance=it} //if instance = null creates database
+        }
+        private fun createDatabase(context: Context) = Room.databaseBuilder(
+                context.applicationContext,
+                MoviesDatabase::class.java,
+                "moviesDB"
+        ).build()
+    }
+}
